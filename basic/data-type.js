d980dcca1bdaa65cc307ab8b3026629b0e410113
akankshar-pixel <raiambu932@gmail.com>
2025-06-22 06:46:28 +0000

    data-type

---
+++ basic/data-type.js
@@ -0,0 +1,29 @@
+"use strict"; //treat new version js
+
+// alert(7+8)  we are using in node js not browser
+
+console.log( 9+5 )
+
+console.log ("akanksha")
+
+let name = "Akanksha"
+
+let age = 5
+
+let isLoggedIN = false
+
+let state;
+
+// number
+// string =>  ""
+// biginit =>
+// boolean  =>  true/false
+//  null => standalon value
+
+// undefiend =>
+
+// symbol => unique
+
+// object =>
+
+    console.log(typeof null);
