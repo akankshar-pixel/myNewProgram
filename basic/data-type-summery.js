3a42e3aa091ff9f483eba482558ce3eca24179a4
akankshar-pixel <raiambu932@gmail.com>
2025-06-24 05:59:02 +0000

    data

---
+++ basic/data-type-summery.js
@@ -0,0 +1,24 @@
+// Primitive data type
+
+// 7 type of data type : String, Number, Boolean, null, undefind, symbol, BigInit
+
+
+// Refrence  ( non primitive )
+
+// array, objects, function
+
+
+let abc = ["orange", "juice","mango" ];
+
+console.log (abc);
+
+const first = function  (){
+
+    console.log ("akanksha");
+
+
+}
+
+
+console.log(typeof abc);
+
