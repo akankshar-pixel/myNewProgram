93da1d3cd65123f965b6dc4c0cf4a64addab80e0
akankshar-pixel <raiambu932@gmail.com>
2025-06-25 04:21:47 +0000

    string.js

---
+++ basic/string.js
@@ -0,0 +1,28 @@
+const abc ="akanksha";
+const xyz = "lovey";
+
+//console.log ( abc + xyz + "value")
+
+console.log(`hi this is akanksha ${abc} and my course is ${xyz}`);
+
+const lori  = new String ('priyambada');
+console.log(lori [0]);
+
+console.log(lori.__proto__);
+
+
+console.log(lori.length);
+console.log(lori.toUpperCase());
+console.log(lori.charAt('4'));
+console.log(lori.indexOf('i'));
+
+const newString = lori.substring(0, 5)
+console.log(newString);
+
+const anotherString = lori.slice(2, 5)
+
+
+const newStringOne = "   hitesh  "
+console.log(newStringOne.trim());
+
+
