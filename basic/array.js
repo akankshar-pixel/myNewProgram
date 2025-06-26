d905a66ac554568a17852694ecd16989cca063c1
akankshar-pixel <raiambu932@gmail.com>
2025-06-26 06:20:41 +0000

    array2.js

---
+++ basic/array.js
@@ -0,0 +1,57 @@
+const myArr = [0, 1, 2, 3, 4];
+
+
+
+
+
+//console.log ( myArr [1]);
+
+// Array Methods 
+// myArr.push(5)
+ // myArr.pop(9)
+
+ //myArr.unshift(8)
+ // add 8 in frist
+
+  //myArr.shift()
+  //remove first text
+
+//console.log(myArr.includes(9))
+//console.log(myArr.indexOf(9))
+
+//const newArr = myArr.join ()
+
+//console.log(myArr);
+//console.log( typeof newArr);
+
+// slice splice
+
+
+
+const mynew = myArr.slice(1,3)
+console.log( mynew)
+console.log("ab", myArr)
+
+
+const myn2 = myArr.splice(1,3)
+console.log("c ", myArr)
+
+console.log(myn2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
