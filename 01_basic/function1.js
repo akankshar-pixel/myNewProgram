7a5f36f78af51d319946de5dc66888e9aa74ab00
akankshar-pixel <raiambu932@gmail.com>
2025-06-29 09:48:39 +0000

    function1.js

---
+++ 01_basic/function1.js
@@ -0,0 +1,39 @@
+function  cartprice ( val1, val2, ...num1){
+return  num1
+
+}
+
+//console.log (cartprice(100, 400,500))
+
+const users = {
+    username : "anu",
+    price : 200,
+ 
+}
+
+function handleobject(anyobject){
+console.log(`user name is ${anyobject.username} and price is ${anyobject.price}`);
+
+}
+
+//handleobject(users)
+
+handleobject({
+
+    username: "akanksha",
+    price: 200
+
+})
+
+
+const myNewarray = [200, 400, 600]
+
+function returnSecondvalue(getArray){
+
+return  getArray[1]
+
+}
+//console.log(returnSecondvalue (myNewarray))
+
+console.log(returnSecondvalue ([200, 400, 600]))
+
