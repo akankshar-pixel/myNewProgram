97d7b26213c1c10a1259bfe163c5c76245268f2b
akankshar-pixel <raiambu932@gmail.com>
2025-06-30 07:29:44 +0000

    scope.js

---
+++ 01_basic/scope.js
@@ -0,0 +1,61 @@
+let a = 300
+
+if(true){
+
+    let a = 10
+    const b = 50
+    //console.log("hi this is akanksha", a)
+   } 
+//console.log(a)
+
+
+//nested function 
+
+function one(){
+const username = "akanksha"
+
+function two(){
+const website = "youtube"
+console.log(username);
+
+}
+
+//console.log(website);
+//two()
+
+}
+//one()
+
+
+
+
+if(true){
+
+    const username = "priya"
+    if(username === "priya"){
+    const website = "youtube"
+  //console.log(username + website);
+
+    }
+    //console.log(website);
+
+}     
+ //console.log(username);
+
+
+ //+++++++++++++++++++++++++++++++++++++++++ intersting +++++++++++++++++++++++++++++//
+
+ addone(5)
+
+function addone (num){
+ return num + 1 
+}
+
+
+ const addTwo = function (num){
+ return num +2
+ }
+
+ addTwo(5)
+
+
