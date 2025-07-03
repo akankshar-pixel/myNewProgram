750f628396d96c789ac939297e79eaeeffc91e3a
akankshar-pixel <raiambu932@gmail.com>
2025-07-03 10:25:11 +0000

    iife-function.js

---
+++ 01_basic/iife-function.js
@@ -0,0 +1,47 @@
+(function chai1 (){
+
+console.log("akanksha kys")
+}) ();
+
+
+
+(function abc (){
+
+    //name iife
+    console.log("priya rai")
+})
+();
+
+
+// arrow iife
+
+( ()  =>  {
+
+console.log ("tffrtyrytry");
+
+}) ();
+
+
+( (name)  =>  {
+
+console.log (` anu ${name} `);
+
+}) ("anju");
+
+
+
+
+
+(function chai(){
+
+    console.log("db connected")
+
+})();
+
+
+( (one)   =>  {
+
+console.log(`db connect2 ${one}  `)
+
+
+})("db connected3")
