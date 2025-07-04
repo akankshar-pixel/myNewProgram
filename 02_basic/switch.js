e4bb63302d523012baa8390bdccdfc44873cdea6
akankshar-pixel <raiambu932@gmail.com>
2025-07-04 06:12:32 +0000

    switch.js

---
+++ 02_basic/switch.js
@@ -0,0 +1,30 @@
+//switch (key) {
+   // case value:
+        
+       // break;
+
+    //default:
+      //  break;
+//}
+
+const month = 'march'
+switch (month) {
+    case 'jan':
+        console.log("janaury")
+        break;
+    case 'feb':
+        console.log("feb")
+        break;
+    case 'march':
+        console.log("March")
+        break;
+    case 'apr':
+        console.log("Apr")
+        break;
+
+    default:
+        console.log('default cast match')
+        break;
+}
+
+
