456ca96749dd6b03dfd4738b9c14c2741b59d5f4
akankshar-pixel <raiambu932@gmail.com>
2025-07-04 05:52:46 +0000

    one.js

---
+++ 02_basic/one.js
@@ -0,0 +1,74 @@
+// if
+
+//const loggedin = true
+
+//const temprutre = 41
+
+//if (temprutre === 41) {
+
+	//console.log("less then 50")
+
+//} else {
+	//console.log(" greter then 50")
+
+//}
+
+//console.log("excute")
+
+//const score = 200
+//if (score > 100) {
+	//const power = "fly"
+	//console.log(`user power : ${power}`);
+
+//}
+//console.log(`user power : ${power}`)
+
+
+//const balance = 1000
+//if(balance > 500) console.log("test"),console.log("tast2");
+
+//if (balance < 500) {
+
+	//console.log("less then 500");
+//} else if (balance < 700) {
+
+	//console.log("less then 700");
+
+
+//} else if (balance < 900) {
+
+	//console.log("less then 900");
+
+
+//} else {
+
+//	console.log("less then 1200")
+//}
+
+
+const userlogged = true
+const debitcard = true
+const loggedinfromgoogle = false
+const loggedinfromemail = true
+
+
+if(userlogged && debitcard  && 2==3 ){
+
+	console.log("Allow to buy cource")
+
+}if(loggedinfromgoogle || loggedinfromemail ){
+
+	console.log("user logged in")
+
+}
+
+
+
+
+
+
+
+
+
+
+
