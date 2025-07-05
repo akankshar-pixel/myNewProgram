de8cf854994b728ca04f98f41459c08373794892
akankshar-pixel <raiambu932@gmail.com>
2025-07-05 10:01:41 +0000

    two.js

---
+++ control-file/two.js
@@ -0,0 +1,30 @@
+let index = 0
+while( index <= 10){
+
+  //  console.log(` value of index is ${index}`)
+    index = index +2
+}
+
+
+
+let myArr = ["akanksha", "priya", "rak"]
+
+let arr = 0
+while(arr <=myArr.length){
+console.log(`my value is ${myArr [arr]}`);
+arr =  arr+1
+
+}
+
+
+
+
+// do  while
+
+let score = 11
+
+do{
+
+  console.log(`score is ${score}`);
+  score++
+} while(score<=10)
